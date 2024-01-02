690849651171491d25b78a125ef5dafb0176deb9
Emin Şahin Oğuz Vurgun Mektepli <128241291+Sahin-Mektepli@users.noreply.github.com>
2024-01-02 12:30:47 +0300

    Emin frame ve panel'lerle ilgili bazı denemelerde bulunuyor. İpek de izliyor.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,38 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
 
+public class Main {
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        JLabel doctor = new JLabel("doktor");
+        JLabel nurse = new JLabel("hemş");
+
+        panel.add(doctor);
+        JTextField textField = new JTextField("Enter Doctor ID");
+        panel.add(textField);
+        panel.add(nurse);
+        JTextField textField2 = new JTextField("Enter Nurse ID");
+        JTextField textField3 = new JTextField("deneme3");
+        JTextField textField4 = new JTextField("deneme4");
+
+        panel.add(textField2);
+        panel.add(textField3);
+        panel.add(textField4);
+
+
+
+
+        frame.add(panel);
+
+
+
+
+        frame.setMinimumSize(new Dimension(100,100));
+        panel.setLayout(new GridLayout(3,2));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
 }
